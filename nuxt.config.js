807da593d9434fe02328c68dd19bddf4b7929306
Matthias Benoit <matthias@danka.fr>
2024-10-24 16:04:37 +0200

    :rocket: [PWA] Add PWA

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss'],  // Ajout de Pinia
+  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', '@nuxtjs/pwa'],  // Ajout de Pinia
   plugins: ['~/plugins/firebase.js', '~/plugins/fontawesome.js'],  // Référence des plugins Firebase et FontAwesome
   head: {
-    link: [{ rel: 'manifest', href: '/manifest.json' }]
+    link: [
+      { rel: 'manifest', href: '/manifest.json' }
+      { rel: 'icon', href: '/icon.png', sizes: '512x512', type: 'image/png' }
+    ]
   },
   tailwindcss: {
     cssPath: ['~/assets/css/tailwind.css', { injectPosition: 'first' }],
@@ -21,5 +24,17 @@ export default defineNuxtConfig({
       './App.{js,ts,vue}',
       './app.{js,ts,vue}',
     ],
+  },
+  pwa: {
+    manifest: {
+      name: 'ToDo App',
+      short_name: 'ToDo',
+      description: 'Une application de gestion de tâches',
+      theme_color: '#000000'
+    },
+    workbox: {
+      // Configuration pour les fonctionnalités hors ligne
+      offline: true
+    }
   },
 });
