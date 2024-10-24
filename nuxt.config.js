ecc7c6b466c2ce72239eb826191c527cb2173401
Matthias Benoit <matthias@danka.fr>
2024-10-24 16:16:51 +0200

    :fix: [PWA] Change package pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,40 +1,34 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', '@nuxtjs/pwa'],  // Ajout de Pinia
-  plugins: ['~/plugins/firebase.js', '~/plugins/fontawesome.js'],  // Référence des plugins Firebase et FontAwesome
+  modules: [
+    '@pinia/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@vite-pwa/nuxt'  // Nouveau module PWA compatible avec Nuxt 3
+  ],
+  plugins: ['~/plugins/firebase.js', '~/plugins/fontawesome.js'],
   head: {
     link: [
       { rel: 'manifest', href: '/manifest.json' },
       { rel: 'icon', href: '/icon.png', sizes: '512x512', type: 'image/png' }
     ]
   },
-  tailwindcss: {
-    cssPath: '~/assets/css/tailwind.css',  // Corrigé en une seule chaîne de caractères
-    configPath: 'tailwind.config',
-    exposeConfig: { level: 2 },
-    config: {},
-    viewer: true,
-    content: [
-      './components/**/*.{vue,js}',
-      './layouts/**/*.vue',
-      './pages/**/*.vue',
-      './composables/**/*.{js,ts}',
-      './plugins/**/*.{js,ts}',
-      './App.{js,ts,vue}',
-      './app.{js,ts,vue}',
-    ],
-  },
   pwa: {
     manifest: {
       name: 'ToDo App',
       short_name: 'ToDo',
       description: 'Une application de gestion de tâches',
-      theme_color: '#000000'
+      theme_color: '#000000',
+      icons: [
+        {
+          src: '/icon.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
     },
     workbox: {
-      // Configuration pour les fonctionnalités hors ligne
-      offline: true
+      offline: true, // Activer les fonctionnalités hors ligne
     }
-  },
+  }
 });
