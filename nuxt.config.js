423f69bd3fa2f7367d97f948c3fb5da42d980a04
Matth-Ben <matthiasbenoit.pro@gmail.com>
2024-10-24 16:41:01 +0200

    :bug: [PWA] Fix pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,48 +10,32 @@ export default defineNuxtConfig({
   head: {
     link: [
       { rel: 'manifest', href: '/manifest.json' },
-      { rel: 'icon', href: '/icon.png', sizes: '512x512', type: 'image/png' }
+      { rel: 'icon', href: '/icon-512.png', sizes: '512x512', type: 'image/png' }
     ]
   },
   pwa: {
     manifest: {
       name: 'ToDo App',
       short_name: 'ToDo',
-      description: 'Une application de gestion de tâches',
+      start_url: '/',
+      display: 'standalone',
       theme_color: '#000000',
       icons: [
         {
-          src: '/icon.png',
+          src: '/icon-192.png',
           sizes: '192x192',
           type: 'image/png'
         },
         {
-          src: '/icon.png',
+          src: '/icon-512.png',
           sizes: '512x512',
           type: 'image/png'
         }
       ]
     },
     workbox: {
-      mode: 'production',  // Mode production pour générer le service worker
-      navigateFallback: '/index.html',  // Redirection pour les pages hors ligne
-      runtimeCaching: [
-        {
-          urlPattern: /^https:\/\/firestore\.googleapis\.com\/.*/,  // Exemple pour Firestore
-          handler: 'NetworkFirst',
-          options: {
-            cacheName: 'api-cache',
-            networkTimeoutSeconds: 10,
-            expiration: {
-              maxEntries: 50,
-              maxAgeSeconds: 30 * 24 * 60 * 60, // 30 jours
-            },
-            cacheableResponse: {
-              statuses: [0, 200]
-            }
-          }
-        }
-      ]
+      mode: 'production',
+      navigateFallback: '/index.html',
     }
   },
 });
